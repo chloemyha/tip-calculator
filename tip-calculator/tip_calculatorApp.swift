baf2b684729d6573819da47b63bd1aa6c2edf951
chloemyha <chloehuynh2401@gmail.com>
2022-11-22 12:26:10 -0500

    Initial Commit

---
+++ tip-calculator/tip_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tip_calculatorApp.swift
+//  tip-calculator
+//
+//  Created by Chloe Huynh on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct tip_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
